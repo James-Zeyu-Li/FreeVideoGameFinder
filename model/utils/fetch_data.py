3eb18525570a36a0fcf4e445d07a94845301fdcc
James_ZY_L <zeyujames@gmail.com>
2024-05-18 23:50:37 -0700

    Add try catch phrase for potential errors
    HTTPError
    ConnectionError
    Timeout Error
    TooManyRedirects error

--- model/utils/fetch_data.py
+++ model/utils/fetch_data.py
@@ -3,12 +3,40 @@ This file fetches data from the API
 """
 
 import requests
+from requests.exceptions import (HTTPError, ConnectionError, Timeout,
+                                 TooManyRedirects)
 
 
 def fetch_games_list(url):
-    response = requests.get(url)
-    response.raise_for_status()
+    """
+    Fetches and returns a list of games.
 
-    if response.status_code == 200:
-        data = response.json()
-        return data
+    Args:
+        url (str): The URL from which to fetch the games list.
+
+    Returns:
+        list of dict: A list of dictionaries where each dictionary contains
+                    data for a single game.
+
+    Raises:
+        HTTPError: 404 Not Found or 500 Internal Server Error.
+        ConnectionError: A connection error occurred.
+        Timeout: The request timed out.
+        TooManyRedirects: Too many redirects.
+    """
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        if response.status_code == 200:
+            data = response.json()
+            return data
+
+    except HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except ConnectionError as conn_err:
+        print(f"Connection error occurred: {conn_err}")
+    except Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except TooManyRedirects as redirect_err:
+        print(f"Redirect error occurred: {redirect_err}")
