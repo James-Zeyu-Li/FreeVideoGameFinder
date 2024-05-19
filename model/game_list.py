b6c3158d3030132f785376d7366ecd5d0fe35e22
James_ZY_L <zeyujames@gmail.com>
2024-05-19 00:39:48 -0700

    Add load model to game list class and also to helper
    The load model from game_list class and have a function which load data to a list. The load model helper function actually creates the list from the API address and call the load model from game_list.

--- model/game_list.py
+++ model/game_list.py
@@ -7,10 +7,43 @@ from random import choice
 from model.utils.clean_data import clean_games_data
 
 
-class Gamelist:
-    # def __init__(self, games=None)
+class GameList:
+    """
+    This class loads game data, selecting random games,
+    searching for games by title, and filtering games by genre.
+    """
 
-    # def load_data(self)
+    def __init__(self, games=None):
+        """
+        Initializes the GameList class.
+
+        Args:
+            games (list, optional): A list of Games.
+
+        Raises:
+            TypeError: If 'games' is not None or a list.
+        """
+        if games is not None and not isinstance(games, list):
+            raise TypeError("games should be None or a list.")
+
+        self.games = games if games is not None else []
+
+    def load_data(self, data):
+        if not isinstance(data, list):
+            raise TypeError("The data is not a list.")
+
+        cleaned_data = clean_games_data(data)
+        for game_data in cleaned_data:
+            game = Game(
+                game_data["title"],
+                game_data["thumbnail"],
+                game_data["short_description"],
+                game_data["game_url"],
+                game_data["genre"],
+                game_data["publisher"],
+                game_data["release_date"]
+            )
+            self.games.append(game)
 
     # def select_random_game(self):
 
