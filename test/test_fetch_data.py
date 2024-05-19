aa59302b49f64240ce20ed1e3e9ed32f770d6a18
James_ZY_L <zeyujames@gmail.com>
2024-05-19 01:34:33 -0700

    add a unit test to the fetch data helper function
    Start to tests API fetch correctly

---
+++ test/test_fetch_data.py
@@ -0,0 +1,47 @@
+"""
+Unit test for fetch data
+"""
+
+
+import requests
+from requests.exceptions import (
+    HTTPError, ConnectionError, Timeout, TooManyRedirects)
+import unittest
+import requests_mock
+
+
+class TestFetchData(unittest.TestCase):
+    """
+    unit tests for the fetch_games_list function
+    """
+
+    def test_fetch_games_list_from_url(self):
+        test_url = "https://www.freetogame.com/api/games?platform=pc"
+
+        excepted_data = {"title": "Overwatch 2",
+                         "thumbnail":
+                         "https:\/\/www.freetogame.com\/g\/540\/thumbnail.jpg",
+                         "short_description":
+                         "A hero-focused first-person team shooter from \
+Blizzard Entertainment.",
+                         "game_url":
+                         "https:\/\/www.freetogame.com\/open\/overwatch-2",
+                         "genre": "Shooter", "platform": "PC (Windows)",
+                         "publisher": "Activision Blizzard",
+                         "release_date": "2022-10-04",
+                         "freetogame_profile_url":
+                         "https:\/\/www.freetogame.com\/overwatch-2"
+                         }
+
+        with requests_mock.Mocker() as m:
+            m.get(test_url, json=excepted_data)
+            response = requests.get(test_url)
+
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.json()['title'], "Overwatch 2")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+# python3 -m unittest test_fetch_data.py
