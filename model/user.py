19769d2da7f48c78746a44fc6046b4646fba8064
James_ZY_L <zeyujames@gmail.com>
2024-06-10 20:12:59 -0700

    Add user file which will start enabling the user login features.

---
+++ model/user.py
@@ -0,0 +1,24 @@
+import re
+
+class User:
+    def __init__(self, username, password):
+        if not isinstance(username, str):
+            raise TypeError('username must be a string')
+
+        self.username = username
+        self.password = password
+        self.liked_games = set()
+
+    @staticmethod
+    def validate_password(self, password):
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        if not re.search(r'[A-Z]', password):
+            raise ValueError("Password must contain at least one uppercase letter")
+        if not re.search(r'[a-z]', password):
+            raise ValueError("Password must contain at least one lowercase letter")
+        if not re.search(r'[0-9]', password):
+            raise ValueError("Password must contain at least one number")
+        if not re.search(r'[\W_]', password):
+            raise ValueError("Password must contain at least one special character")
+
