e347ecd06edcf31fb9d4a58f23d973c7000e06b1
James_ZY_L <zeyujames@gmail.com>
2024-06-11 19:05:47 -0700

    Update user class, change parameter name and add docstring, create user controller, user_namagement and login_view file.

--- model/user.py
+++ model/user.py
@@ -1,8 +1,22 @@
+"""
+A class to help with login and mark favorite.
+"""
 import re
 
 
 class User:
+    """
+    The class to represent a user in a gaming application.
+    """
+
     def __init__(self, username, password):
+        """
+        Constructs all the necessary attributes for the user object.
+
+        Args:
+            user_id (int): The unique identifier for the user.
+            username (str): The username of the user.
+        """
         if not isinstance(username, str):
             raise TypeError('username must be a string')
 
@@ -11,23 +25,42 @@ class User:
         self.logged_in = False
         self.favorite_games = set()
 
-    def login(self):
-        self.logged_in = True
+    def login(self, password):
+        """
+        Sets the user's login status to True.
+        """
+        if self.password == password:
+            self.logged_in = True
+            return True
+        return False
 
     def logout(self):
+        """
+        Sets the user's login statue to be false.
+        """
         self.logged_in = False
 
-    def toggle_favorite(self, game_id=None):
-        if game_id in self.favorite_games:
-            self.favorite_games.remove(game_id)
+    def toggle_favorite(self, username):
+        """
+        Toggle favorite status for given game.
+        if it's already favorite, remove the game.
+        """
+        if username in self.favorite_games:
+            self.favorite_games.remove(username)
         else:
-            self.favorite_games.add(game_id)
+            self.favorite_games.add(username)
 
-    def is_favorite(self, game_id):
-        return game_id in self.favorite_games
+    def is_favorite(self, username):
+        """
+        Check if a game is in user's favorite. 
+        """
+        return username in self.favorite_games
 
     @staticmethod
     def validate_password(self, password):
+        """
+        Set the limitation to the structure of the password.
+        """
         if len(password) < 8:
             raise ValueError("Password must be at least 8 characters long")
         if not re.search(r'[A-Z]', password):
---
+++ model/user_mamagement.py
@@ -0,0 +1,11 @@
+"""
+This class will be used to manage user profile and user register
+"""
+
+class UserManagement:
+    '''
+    
+    '''
+
+    def __init__(self):
+        self.users = {}
