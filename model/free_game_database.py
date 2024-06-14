37cb23a6143cfd423e58063c8aff21bcd201c655
James_ZY_L <zeyujames@gmail.com>
2024-06-13 22:38:55 -0700

    Add free_game_database file to connect to MongoDB for data management

---
+++ model/free_game_database.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+
+class Database:
+    def __init__(self, uri, db_name):
+        self.client = MongoClient(uri)
+        self.db = self.client[db_name]
+
+    def get_collection(self, collection_name):
+        return self.db[collection_name]
+
+    def close(self):
+        self.client.close()
